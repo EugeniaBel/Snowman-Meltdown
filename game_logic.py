1d7c28bac62950fce027eb2200993c0624dfb9f4
EugeniaBel <belohay@gmail.com>
2025-04-17 00:53:00 +0200

    Format code according to PEP 8 guidelines.

--- game_logic.py
+++ game_logic.py
@@ -1,13 +1,16 @@
 import random
+
 from ascii_art import STAGES
 
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 MAX_MISTAKES = len(STAGES) - 1
 
+
 def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
+
 
 def display_game_state(mistakes, secret_word, guessed_letters):
     """Displays the current game state: snowman and guessed word."""
@@ -22,6 +25,7 @@ def display_game_state(mistakes, secret_word, guessed_letters):
     print("\nRemaining mistakes:", MAX_MISTAKES - mistakes)
     print("Guessed letters:", ", ".join(sorted(list(guessed_letters))))
     print("\n")
+
 
 def play_game():
     secret_word = get_random_word()
@@ -62,6 +66,6 @@ def play_game():
     else:
         print("Game Over! The snowman melted. The word was:", secret_word)
 
+
 if __name__ == "__main__":
     play_game()
-
